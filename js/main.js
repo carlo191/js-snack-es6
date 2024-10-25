060b989003622087864ba1368bbdd5a584ccf355
Carlo Elvisi <carloelvisi01@outlook.it>
2024-10-25 15:46:36 +0200

    snack 2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,31 @@ biciclette.forEach((bici) => {
 console.log(
   `La bici con il peso minore è ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg.`
 );
+
+/*Snack 2
+Creare un array di oggetti di squadre di calcio.
+ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà "punti" fatti e "falli subiti".*/
+
+const squadre = [
+  { nome: `SSC Napoli`, puntiFatti: 0, falliSubiti: 0 },
+  { nome: `AS Roma`, puntiFatti: 0, falliSubiti: 0 },
+  { nome: `AC Milan`, puntiFatti: 0, falliSubiti: 0 },
+  { nome: `ACF Fiorentina`, puntiFatti: 0, falliSubiti: 0 },
+];
+
+/**
+ *
+ * @returns un numero casuale tra 1 e 100
+ */
+function generaNumeriRandom() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+squadre.forEach((squadra) => {
+  squadra.puntiFatti = generaNumeriRandom();
+  squadra.falliSubiti = generaNumeriRandom();
+});
+
+console.log(squadre);
